da4d75f25816fbd938fff98ed2a6992244843373
Joost Lek <vlabakje@gmail.com>
2018-11-27 21:24:51 +0100

    simple merge success

---
+++ simple-merge.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+dirname=`mktemp -d`
+cd $dirname
+git init .
+echo "initial file on master" > one.txt
+git add one.txt
+git commit -m 'initial commit master'
+git checkout -b 'overnight'
+echo "initial file only on overnight" > two.txt
+git add two.txt
+git commit -m 'initial commit overnight' two.txt
+echo "merging overnight into master"
+git checkout master
+git merge --no-ff -m 'Merging overnight into master' overnight
+ls -l
+git log
+cd ..
+rm -rf "$dirname"
